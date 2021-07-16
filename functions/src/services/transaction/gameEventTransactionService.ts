5f20cced3e5902080258bad6a383518df2881ab6
Cory Loeffelman <frontenddevemail@gmail.com>
2021-07-16 18:26:52 -0500

    Update matchup side profile on player profile update

--- functions/src/services/transaction/gameEventTransactionService.ts
+++ functions/src/services/transaction/gameEventTransactionService.ts
@@ -3,6 +3,7 @@ import firebase from "firebase-admin";
 import { db } from "../../../firebase";
 
 import { gameEventConverter, IGameEvent } from "../../../../stroll-models/gameEvent/gameEvent";
+import { IMatchup, matchupConverter } from "../../../../stroll-models/matchup";
 import { IProfileReference } from "../../../../stroll-models/profileReference";
 
 import { GameEventType } from "../../../../stroll-enums/gameEventType";
@@ -24,6 +25,11 @@ export const GameEventTransactionService: IGameEventTransactionService = {
   },
   updatePlayerProfile: async (gameID: string, playerID: string, profile: IProfileReference): Promise<void> => {    
     await db.runTransaction(async (transaction: firebase.firestore.Transaction) => {
+      const matchupRef: firebase.firestore.Query = db.collection("games")
+        .doc(gameID)
+        .collection("matchups")
+        .withConverter<IMatchup>(matchupConverter);
+
       const eventsRef: firebase.firestore.Query = db.collection("games")
         .doc(gameID)
         .collection("events") 
@@ -31,7 +37,20 @@ export const GameEventTransactionService: IGameEventTransactionService = {
         .where("profile.uid", "==", playerID)    
         .withConverter(gameEventConverter);
 
-      const eventSnap: firebase.firestore.QuerySnapshot = await transaction.get(eventsRef);
+      const matchupSnap: firebase.firestore.QuerySnapshot = await transaction.get(matchupRef),
+        eventSnap: firebase.firestore.QuerySnapshot = await transaction.get(eventsRef);
+
+      if(!matchupSnap.empty) {
+        matchupSnap.forEach((doc: firebase.firestore.QueryDocumentSnapshot<IMatchup>) => {
+          const matchup: IMatchup = doc.data();
+
+          if(matchup.left.profile.uid === playerID) {
+            transaction.update(doc.ref, { ["left.profile"]: profile });
+          } else if (matchup.right.profile.uid === playerID) {
+            transaction.update(doc.ref, { ["right.profile"]: profile });
+          }
+        });
+      }
 
       if(!eventSnap.empty) {   
         eventSnap.forEach((doc: firebase.firestore.QueryDocumentSnapshot<IGameEvent>) => 
