7f2b75138de82bfde37ade5da0aee1073df88513
Cory Loeffelman <frontenddevemail@gmail.com>
2021-08-05 13:45:02 -0500

    Game Day statement uses animated counter

--- stroll-client/components/gameDayStatement/gameDayStatement.tsx
+++ stroll-client/components/gameDayStatement/gameDayStatement.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 
+import { AnimatedCounter } from "../animatedCounter/animatedCounter";
+
 import { Icon } from "../../../stroll-enums/icon";
 
 interface GameDayStatementProps {  
   quantity: number;
 }
 
-export const GameDayStatement: React.FC<GameDayStatementProps> = (props: GameDayStatementProps) => {      
+export const GameDayStatement: React.FC<GameDayStatementProps> = (props: GameDayStatementProps) => {  
+  const quantity: JSX.Element = (                   
+    <AnimatedCounter 
+      initialValue={props.quantity} 
+      value={props.quantity} 
+      formatValue={(value: number) => value.toLocaleString()} 
+    />
+  )
+  
   return (
-    <span className="game-day-statement"><i className={Icon.OneGameDay} /> {props.quantity} Game Days</span>
+    <span className="game-day-statement"><i className={Icon.OneGameDay} /> {quantity} Game Days</span>
   );
 }
