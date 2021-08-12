bcb7ffa723f1b087f6eb99b2c7ee051efd518bf1
Cory Loeffelman <frontenddevemail@gmail.com>
2021-08-12 10:08:33 -0500

    Removed unused stripe packages/code

--- stroll-client/index.tsx
+++ stroll-client/index.tsx
@@ -2,21 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import "@stripe/stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-
-import { STRIPE_PUBLISHABLE_KEY } from "./config/stripe";
-
 import { App } from "./components/app/app";
 
 require("./components/app/scss/app.scss");
 
 ReactDOM.render(
   <Router>
-    <Elements stripe={loadStripe(STRIPE_PUBLISHABLE_KEY)}>
-      <App />
-    </Elements>
+    <App />
   </Router>,
   document.getElementById("root")
 );
