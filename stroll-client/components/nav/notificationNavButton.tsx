595b4892cb1317fa6cdd895247547ed58cd7b1b3
Cory Loeffelman <frontenddevemail@gmail.com>
2021-08-13 14:18:42 -0500

    Always render notification nav button

--- stroll-client/components/nav/notificationNavButton.tsx
+++ stroll-client/components/nav/notificationNavButton.tsx
@@ -10,35 +10,31 @@ interface NotificationNavButtonProps {
 export const NotificationNavButton: React.FC<NotificationNavButtonProps> = (props: NotificationNavButtonProps) => {  
   const { appState } = useContext(AppContext);
 
-  if(appState.user.profile.uid !== "") {
-    const { stats } = appState.user;
+  const { stats } = appState.user;
 
-    const getNotificationCount = (): JSX.Element => {
-      if(stats.notifications.unviewed > 0) {
-        const getText = (): string | number => {
-          if(stats.notifications.unviewed > 8) {
-            return "9+";
-          }
-
-          return stats.notifications.unviewed;
+  const getNotificationCount = (): JSX.Element => {
+    if(stats.notifications.unviewed > 0) {
+      const getText = (): string | number => {
+        if(stats.notifications.unviewed > 8) {
+          return "9+";
         }
 
-        return (
-          <h1 className="notification-count passion-one-font">{getText()}</h1>
-        )
+        return stats.notifications.unviewed;
       }
-    }
 
-    return (
-      <div className="notification-nav-menu-button-wrapper nav-menu-button-wrapper">
-        <NavLink to="/notifications" className="nav-menu-button" exact>
-          <i className="far fa-bell" />
-          <h1 className="notification-label passion-one-font">Notifications</h1>
-          {getNotificationCount()}
-        </NavLink>
-      </div>
-    );
+      return (
+        <h1 className="notification-count passion-one-font">{getText()}</h1>
+      )
+    }
   }
 
-  return null;
+  return (
+    <div className="notification-nav-menu-button-wrapper nav-menu-button-wrapper">
+      <NavLink to="/notifications" className="nav-menu-button" exact>
+        <i className="far fa-bell" />
+        <h1 className="notification-label passion-one-font">Notifications</h1>
+        {getNotificationCount()}
+      </NavLink>
+    </div>
+  );
 }
