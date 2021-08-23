fe8402208c94b47bdc79a7119113eeda99a50fa0
Cory Loeffelman <frontenddevemail@gmail.com>
2021-08-23 14:00:39 -0500

    Added missing icon, moved svg blobs to cdn

--- stroll-client/components/svgBlob/svgBlob.tsx
+++ stroll-client/components/svgBlob/svgBlob.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import { ImageUtility } from "../../utilities/imageUtility";
+
 interface SvgBlobProps {  
   index: number;
 }
 
 export const SvgBlob: React.FC<SvgBlobProps> = (props: SvgBlobProps) => {  
   return (
-    <img className="svg-blob" src={`/img/blobs/blob-${props.index}.svg`} />
+    <img className="svg-blob" src={ImageUtility.getBlob(props.index)} />
   );
 }
