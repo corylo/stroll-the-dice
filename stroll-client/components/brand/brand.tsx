a15641a8c3b5068c645b3fc9c43caab958629347
Cory Loeffelman <frontenddevemail@gmail.com>
2021-08-23 13:32:09 -0500

    Updated site logo and icons

--- stroll-client/components/brand/brand.tsx
+++ stroll-client/components/brand/brand.tsx
@@ -1,27 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { Icon } from "../../../stroll-enums/icon";
-
 interface BrandProps {
   showBrandText?: boolean;
 }
 
 export const Brand: React.FC<BrandProps> = (props: BrandProps) => {
-  const getBrandText = (): JSX.Element | null => {
-    if(props.showBrandText !== undefined && props.showBrandText === false) {
-      return null;
-    }
-
-    return (
-      <h1 className="app-brand-name lobster-font">Stroll The Dice</h1>
-    );
-  }
-
   return(
     <Link className="app-brand" to="/">
-      <i className={Icon.AppBrand} />
-      {getBrandText()}
+      <img src="/img/logo.png" />
     </Link>
   )
 }
