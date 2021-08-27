3fcd4db7640e1c927924dd98aa8df973c21124c1
Cory Loeffelman <frontenddevemail@gmail.com>
2021-08-27 12:53:20 -0500

    Updated learn more image

--- stroll-client/components/learnMoreLink/learnMoreLink.tsx
+++ stroll-client/components/learnMoreLink/learnMoreLink.tsx
@@ -10,7 +10,7 @@ interface LearnMoreLinkProps {
 
 export const LearnMoreLink: React.FC<LearnMoreLinkProps> = (props: LearnMoreLinkProps) => {  
   return (
-    <Button className="learn-more-link" url="/how-to-play#goal" styles={{ backgroundImage: `url(${ImageUtility.getGraphic("park", "png")})` }}>
+    <Button className="learn-more-link" url="/how-to-play#goal" styles={{ backgroundImage: `url(${ImageUtility.getGraphic("learn-more", "png")})` }}>
       <div className="learn-more-link-content">
         <i className="far fa-question" />
         <h1 className="passion-one-font">{props.text || "Learn more"}</h1>
