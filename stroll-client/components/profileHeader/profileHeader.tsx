33fa9a28d9b3b8cea831f1f60828a1c8a60762e6
Cory Loeffelman <frontenddevemail@gmail.com>
2021-09-10 14:22:36 -0500

    Removing email disclaimer text on profile page

--- stroll-client/components/profileHeader/profileHeader.tsx
+++ stroll-client/components/profileHeader/profileHeader.tsx
@@ -24,7 +24,7 @@ export const ProfileHeader: React.FC<ProfileHeaderProps> = (props: ProfileHeader
           return (
             <div className="profile-email">
               <h1 className="profile-email-text passion-one-font">{user.email}</h1>
-              <h1 className="profile-email-disclaimer passion-one-font">(only you can see)</h1>
+              {/* <h1 className="profile-email-disclaimer passion-one-font">(only you can see)</h1> */}
               <PlayerLevelBadge 
                 clickable
                 experience={user.profile.experience} 
