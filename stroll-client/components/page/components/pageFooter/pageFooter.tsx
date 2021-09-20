687b97013d0cb6ff7f20f00f0f1659baf39c0947
Cory Loeffelman <frontenddevemail@gmail.com>
2021-09-20 10:59:02 -0500

    Updated page footer

--- stroll-client/components/page/components/pageFooter/pageFooter.tsx
+++ stroll-client/components/page/components/pageFooter/pageFooter.tsx
@@ -10,22 +10,27 @@ interface PageFooterProps {
 export const PageFooter: React.FC<PageFooterProps> = (props: PageFooterProps) => {  
   return (
     <div className="page-footer">
-      <PageFooterSection title="Legal">
+      <div className="page-footer-sections">
+        <PageFooterSection title="Legal">
+            <div className="page-footer-link">
+              <a className="passion-one-font" href="https://legal.strollthedice.com/privacy-policy" target="_blank">Privacy Policy</a>
+            </div>
+            <div className="page-footer-link">
+              <a className="passion-one-font" href="https://legal.strollthedice.com/terms-and-conditions" target="_blank">Terms & Conditions</a>
+            </div>
+            <div className="page-footer-link">
+              <a className="passion-one-font" href="https://legal.strollthedice.com/cookie-policy" target="_blank">Cookie Policy</a>
+            </div>
+        </PageFooterSection>
+        <PageFooterSection title="Support">
           <div className="page-footer-link">
-            <a className="passion-one-font" href="https://legal.strollthedice.com/privacy-policy" target="_blank">Privacy Policy</a>
+            <Link className="passion-one-font" to="/contact-us">Contact Us</Link>
           </div>
-          <div className="page-footer-link">
-            <a className="passion-one-font" href="https://legal.strollthedice.com/terms-and-conditions" target="_blank">Terms & Conditions</a>
-          </div>
-          <div className="page-footer-link">
-            <a className="passion-one-font" href="https://legal.strollthedice.com/cookie-policy" target="_blank">Cookie Policy</a>
-          </div>
-      </PageFooterSection>
-      <PageFooterSection title="Support">
-        <div className="page-footer-link">
-          <Link className="passion-one-font" to="/contact-us">Contact Us</Link>
-        </div>
-      </PageFooterSection>
+        </PageFooterSection>
+      </div>
+      <div className="page-footer-rights-statement">
+        <h1 className="passion-one-font">{new Date().getFullYear()} Waff Apps LLC. All rights reserved.</h1>
+      </div>
     </div>
   );
 }
