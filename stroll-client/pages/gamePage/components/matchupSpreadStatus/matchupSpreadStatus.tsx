42846e9490074324452bc9a5dc37a198cee8734d
Cory Loeffelman <frontenddevemail@gmail.com>
2021-09-22 15:48:25 -0500

    Spread status bar

--- stroll-client/pages/gamePage/components/matchupSpreadStatus/matchupSpreadStatus.tsx
+++ stroll-client/pages/gamePage/components/matchupSpreadStatus/matchupSpreadStatus.tsx
@@ -35,7 +35,12 @@ export const MatchupSpreadStatus: React.FC<MatchupSpreadStatusProps> = (props: M
   }
 
   if(matchup.right.playerID !== "") {
-    const getContent = (): JSX.Element => {      
+    const favoriteSide: IMatchupSide = MatchupUtility.getSideByFavorite(matchup, true),
+      underdogSide: IMatchupSide = MatchupUtility.getSideByFavorite(matchup, false),
+      favoritePlayerStatement: JSX.Element = <PlayerStatement profile={favoriteSide.profile} />,        
+      spreadStatement: JSX.Element = <IconStatement icon={Icon.Steps} text={matchup.spread.toLocaleString()} />;
+
+    const getBodyContent = (): JSX.Element => {      
       if(matchup.spreadCreatedAt === null) {
         return (
           <h1 className="matchup-spread-status-description passion-one-font">The spread has not been determined yet.</h1>
@@ -45,11 +50,6 @@ export const MatchupSpreadStatus: React.FC<MatchupSpreadStatusProps> = (props: M
           <h1 className="matchup-spread-status-description passion-one-font">There is no favorite for this matchup since one or more players does not have enough step history yet.</h1>
         )
       }
-
-      const favoriteSide: IMatchupSide = MatchupUtility.getSideByFavorite(matchup, true),
-        underdogSide: IMatchupSide = MatchupUtility.getSideByFavorite(matchup, false),
-        favoritePlayerStatement: JSX.Element = <PlayerStatement profile={favoriteSide.profile} />,        
-        spreadStatement: JSX.Element = <IconStatement icon={Icon.Steps} text={matchup.spread.toLocaleString()} />;
 
       if(dayStatus === GameStatus.Upcoming) {
         return (
@@ -84,21 +84,77 @@ export const MatchupSpreadStatus: React.FC<MatchupSpreadStatusProps> = (props: M
       }
     }
 
+    const getSpreadStatusBar = (): JSX.Element => {
+      if(dayStatus !== GameStatus.Upcoming) {
+        const spreadCoverageDecimal: number = (favoriteSide.steps / (matchup.spread + 1)),
+          spreadCoveragePercentage: number = Math.min(100, spreadCoverageDecimal * 100);
+        
+        const spreadCoverageRoundedPercentage: string = (spreadCoveragePercentage === 0 || spreadCoveragePercentage === 100)
+            ? spreadCoveragePercentage.toString()
+            : spreadCoveragePercentage.toFixed(2);
+        
+        const favoriteStyles: React.CSSProperties = {
+          backgroundColor: `rgb(${favoriteSide.profile.color})`,
+          width: `${spreadCoveragePercentage}%`
+        }
+
+        const underdogStyles: React.CSSProperties = {
+          backgroundColor: `rgb(${underdogSide.profile.color})`,
+          width: `${(1 - spreadCoverageDecimal) * 100}%`
+        }
+
+        const getSpreadStatusBarPercentage = (): JSX.Element => {
+          const styles: React.CSSProperties = { left: `${spreadCoveragePercentage}%` };
+
+          if(spreadCoveragePercentage > 50) {
+            return (
+              <div className="matchup-spread-status-bar-percentage right-side" style={styles}>
+                <h1 className="passion-one-font">{spreadCoverageRoundedPercentage}%</h1>
+                <i className="player-level-badge-experience-bar-indicator fas fa-map-marker" />
+              </div>
+            )
+          }
+
+          return (
+            <div className="matchup-spread-status-bar-percentage left-side" style={styles}>
+              <i className="player-level-badge-experience-bar-indicator fas fa-map-marker" />
+              <h1 className="passion-one-font">{spreadCoverageRoundedPercentage}%</h1>
+            </div>
+          )
+        }
+
+        return (        
+          <div className="matchup-spread-status-bar">
+            <div className="matchup-spread-status-bar-percentage-track">
+              {getSpreadStatusBarPercentage()}
+            </div>
+            <div className="matchup-spread-status-bar-halves">
+              <div className="matchup-spread-status-bar-half favorite" style={favoriteStyles} />
+              <div className="matchup-spread-status-bar-half underdog" style={underdogStyles} />          
+            </div>
+          </div>
+        )
+      }
+    }
+
     return (
       <div className="matchup-spread-status">
-        <div className="matchup-spread-status-label">
-          <div className="matchup-spread-status-label-body">
-            <i className={Icon.Spread} />
-            <h1 className="passion-one-font">Spread</h1>
+        <div className="matchup-spread-status-content">
+          <div className="matchup-spread-status-label">
+            <div className="matchup-spread-status-label-body">
+              <i className={Icon.Spread} />
+              <h1 className="passion-one-font">Spread</h1>
+            </div>
+            <IconButton
+              icon="fal fa-info-circle"
+              handleOnClick={toggle}
+            />
           </div>
-          <IconButton
-            icon="fal fa-info-circle"
-            handleOnClick={toggle}
-          />
+          <div className="matchup-spread-status-body">
+            {getBodyContent()}
+          </div>
         </div>
-        <div className="matchup-spread-status-body">
-          {getContent()}
-        </div>
+        {getSpreadStatusBar()}
       </div>
     );
   }
