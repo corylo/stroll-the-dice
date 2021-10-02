5c79b8a7d0ff2b100dd9321bec7874996a6cb8f0
Cory Loeffelman <frontenddevemail@gmail.com>
2021-10-02 11:59:07 -0500

    Only show confetti if signed in

--- stroll-client/pages/gamePage/components/gamePageContent/gamePageContent.tsx
+++ stroll-client/pages/gamePage/components/gamePageContent/gamePageContent.tsx
@@ -177,7 +177,7 @@ export const GamePageContent: React.FC<GamePageContentProps> = (props: GamePageC
     const getConfetti = (): JSX.Element => {
       const endOfFinalDayUpdateComplete: boolean = FirestoreDateUtility.endOfDayProgressUpdateComplete(game.duration, game.startsAt, game.progressUpdateAt);
 
-      if(game.status === GameStatus.Completed && endOfFinalDayUpdateComplete) {
+      if(appState.status === AppStatus.SignedIn && game.status === GameStatus.Completed && endOfFinalDayUpdateComplete) {
         return (
           <Confetti />
         );
